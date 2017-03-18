25b2e5f20ddabf4ad52ecbd8990400c2cd9d653d
lordoftheflies <heglas11@gmail.com>
2017-03-18 19:01:38 +0100

    Revert "My version."
    This reverts commit 89e66d22644eff70402dfcd38dab0e767c6465e1.

--- setup.py
+++ setup.py
@@ -28,11 +28,11 @@ long_description = '\n\n'.join([read('README'),
 
 __doc__ = long_description
 
-requirements = ['stringparser', 'hedgehog-pyvisa>=2.0.0', 'pyyaml']
+requirements = ['stringparser', 'pyvisa>=1.8', 'pyyaml']
 
-setup(name='hedgehog-pyvisa-sim',
+setup(name='PyVISA-sim',
       description='Simulated backend for PyVISA implementing TCPIP, GPIB, RS232, and USB resources',
-      version='0.5',
+      version='0.4.dev0',
       long_description=long_description,
       author='Hernan E. Grecco',
       author_email='hernan.grecco@gmail.com',
